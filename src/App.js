7c52efcf3b0fb8fc7587129fef17415bbd5873af
jhonatanquirama <quiramapino47@gmail.com>
2020-09-13 10:48:06 -0500

    actualizaciones

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Calc from './Components/Calc';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculadora App</h1>
+      <Calc/>
+    </div>
+  );
+}
+
+export default App;
